8276539f5598dff55fe27fcce6acc8edc5c5cc29
sejin_ <sejin2740@korea.ac.kr>
2020-09-16 16:17:11 +0900

    RecyclerView +

--- app/src/main/java/com/example/howmany/MainActivity.java
+++ app/src/main/java/com/example/howmany/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.howmany;
 
-import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,14 +15,16 @@ import com.google.zxing.integration.android.IntentResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static java.security.AccessController.getContext;
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTAG";
     //view Objects
     private Button buttonScan;
     private TextView textViewName, textViewAddress, textViewResult;
 
     //qr code scanner object
     private IntentIntegrator qrScan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
         //View Objects
         buttonScan = (Button) findViewById(R.id.buttonScan);
-        textViewName = (TextView) findViewById(R.id.textViewName);
-        textViewAddress = (TextView) findViewById(R.id.textViewAddress);
-        textViewResult = (TextView)  findViewById(R.id.textViewResult);
-
         //intializing scan object
         qrScan = new IntentIntegrator(this);
 
         //button onClick
+        // qr코드 스캐너 버튼시
         buttonScan.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //scan option
@@ -47,25 +46,47 @@ public class MainActivity extends AppCompatActivity {
                 qrScan.initiateScan();
             }
         });
+
+
+
     }
 
     //Getting the scan results
+
+    //qr코드 승인 , qr코드 없을시
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
-            //qrcode 가 없으면
+           //qrcode 가 없으면
+           /*String address = "www.naver.com"; //obj.getString("address");// 주소 받아오기
+            Intent intent_1 = new Intent(MainActivity.this, pWebView.class);
+            intent_1.putExtra("webview_addr",address);
+            startActivity(intent_1);
+            */
             if (result.getContents() == null) {
-                Toast.makeText(MainActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"테스트0");
+                String address = "http://www.naver.com"; //obj.getString("address");// 주소 받아오기
+                Intent intent_1 = new Intent(MainActivity.this, pWebView.class);
+                intent_1.putExtra("webview_addr",address);
+                startActivity(intent_1);
+                Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+
             } else {
                 //qrcode 결과가 있으면
-                Toast.makeText(MainActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"테스트1");
+                Toast.makeText(MainActivity.this, "Scan Perfect", Toast.LENGTH_SHORT).show();
                 try {
                     //data를 json으로 변환
+                    Log.d(TAG,"테스트2");
                     JSONObject obj = new JSONObject(result.getContents());
                     textViewName.setText(obj.getString("name"));
                     textViewAddress.setText(obj.getString("address"));
-                    String address = obj.getString("address");// 주소 받아오기
+                    String address_1 = "m.naver.com"; //obj.getString("address");// 주소 받아오기
+                    Intent intent = new Intent(MainActivity.this, pWebView.class);
+                    intent.putExtra("webview_addr",address_1);
+
+                    startActivity(intent);
                 } catch (JSONException e) {
                     e.printStackTrace();
                     //Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_LONG).show();
