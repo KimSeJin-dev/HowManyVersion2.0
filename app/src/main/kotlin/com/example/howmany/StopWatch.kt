9d64ef3e1b789d4a88e79b53e1491742a6405be2
sejin_ <sejin2740@korea.ac.kr>
2020-09-29 17:10:21 +0900

    +another data plus + time + stop watch

---
+++ app/src/main/kotlin/com/example/howmany/StopWatch.kt
@@ -0,0 +1,94 @@
+package com.example.howmany
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_stop_watch.*
+import java.util.*
+import kotlin.concurrent.timer
+
+class StopWatch : AppCompatActivity() {
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var Lap = 1
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stop_watch)
+
+        lapButton.setOnClickListener {
+            recordLapTime()
+        }
+
+        resetFab.setOnClickListener {
+            reset()
+        }
+
+
+        fab.setOnClickListener {
+            isRunning = !isRunning
+
+            if(isRunning){
+                start()
+            } else {
+                pause()
+            }
+        }
+    }
+
+    private fun recordLapTime(){
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,40F)
+        textView.setTextColor(Color.parseColor("#FF7200"))
+        textView.text = "$Lap LAB : ${lapTime / 100}" + "분 " + "${lapTime % 100}" + "초"
+         //맨 위에 랩타임 추가가
+        lapLayout.addView(textView, 0)
+        Lap++
+    }
+
+
+    private fun pause(){
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        timerTask?.cancel()
+    }
+
+    private fun start(){
+        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+
+        timerTask = timer(period = 10){
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+
+        }
+
+    }
+
+    private fun reset(){
+        timerTask?.cancel()
+
+        //모든 변수 초기화
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+
+        lapLayout.removeAllViews()
+        Lap = 1
+    }
+
+
+
+
+}
