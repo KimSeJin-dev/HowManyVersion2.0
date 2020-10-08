28d10a42090eda3c40ad48a17af5b93e6cf6448c
sejin_ <sejin2740@korea.ac.kr>
2020-10-08 17:21:10 +0900

    change date

--- app/src/main/java/com/example/howmany/FragFirst.java
+++ app/src/main/java/com/example/howmany/FragFirst.java
@@ -2,6 +2,7 @@ package com.example.howmany;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import im.dacer.androidcharts.LineView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FragFirst extends Fragment {
+
+    private String TAG = "TAG";
+    private MyAPI mMyAPI;
+    private final String BASE_URL = "http://emoclew.pythonanywhere.com";
+
 
     @Nullable
     @Override
@@ -25,8 +42,7 @@ public class FragFirst extends Fragment {
         );
 
         final LineView lineViewFloat = (LineView) rootView.findViewById(R.id.line_view_float);
-
-
+        initMyAPI(BASE_URL);
        initLineView(lineViewFloat);
        randomSet(lineViewFloat);
         return rootView;
@@ -55,6 +71,33 @@ public class FragFirst extends Fragment {
 
     private void randomSet(LineView lineViewFloat) {
 
+        Call<List<PostItem>> getCall = mMyAPI.get_posts();
+        getCall.enqueue(new Callback<List<PostItem>>() {
+            @Override
+            public void onResponse(Call<List<PostItem>> call, Response<List<PostItem>> response) {
+                if (response.isSuccessful()) {
+                    List<PostItem> mList = response.body();
+
+                    for (PostItem item : mList) {
+                        String day = item.getEnter_time().substring(0,10);
+                        String Date  = getDateDay(day);
+
+
+                    }
+
+                } else {
+                    Log.d(TAG, "Status Code : " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PostItem>> call, Throwable t) {
+
+            }
+        });
+
+
+
         ArrayList<Integer> dataListF = new ArrayList<>();
         Random r = new Random();
         for (int i = 0; i < 9; i++) {
@@ -66,4 +109,64 @@ public class FragFirst extends Fragment {
 
         lineViewFloat.setDataList(dataListFs);
     }
+
+    private String getDateDay(String day){
+        String day_x ="";
+
+        String day_num[] = day.split("-");
+        String day_final = day_num[0] + day_num[1] + day_num[2];
+
+        java.util.Date wantDate = null;
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyyMMdd");
+
+        try {
+            wantDate = sdf.parse(day_final);
+        }
+        catch (ParseException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(wantDate);
+        int final_date = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch(final_date){
+            case 1:
+                day_x = "일";
+                break ;
+            case 2:
+                day_x = "월";
+                break ;
+            case 3:
+                day_x = "화";
+                break ;
+            case 4:
+                day_x = "수";
+                break ;
+            case 5:
+                day_x = "목";
+                break ;
+            case 6:
+                day_x = "금";
+                break ;
+            case 7:
+                day_x = "토";
+                break ;
+        }
+
+        return day_x;
+
+    }
+
+
+    private void initMyAPI(String baseUrl){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        mMyAPI = retrofit.create(MyAPI.class);
+    }
+
 }
