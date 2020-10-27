2d710b20b56788f752d3ddc6d374e85b5e49a8dd
kimsejin <sejin2740@korea.ac.kr>
2020-10-28 01:08:34 +0900

    live count

---
+++ app/src/main/java/com/example/howmany/FirstInput.java
@@ -0,0 +1,52 @@
+package com.example.howmany;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class FirstInput extends AppCompatActivity {
+
+    private AlertDialog dialog;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_input);
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View v = inflater.inflate(R.layout.login_dialog, null);
+
+        editTextEmail = (EditText) v.findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) v.findViewById(R.id.editTextPassword);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(v);
+        builder.setPositiveButton("입력", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                Toast.makeText(FirstInput.this, "Email : "+email+"\nPassword : "+password, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(FirstInput.this, MainActivity.class);
+                startActivity(intent);
+//                Toast.makeText(this, "Email : "+email+"\nPassword : "+password, Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+//        dialog = builder.create();
+//        dialog.show();
+    }
+
+}
